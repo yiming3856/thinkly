3d0c126f6d94cee297b750638d97d1c7fb7d500b
yiming3856 <3638168@proton.me>
2025-07-03 09:11:47 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://thinkly.cc/", // replace this with your deployed domain
+  author: "Notink",
   profile: "https://thinkly.cc/",
   desc: "欢迎来到我的个人博客，在这个博客中，我将写下我的项目（我做什么/我如何做）、我的个人经历以及一些随机的东西⭐",
   title: "Notink",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 11,
+  postPerPage: 11,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
